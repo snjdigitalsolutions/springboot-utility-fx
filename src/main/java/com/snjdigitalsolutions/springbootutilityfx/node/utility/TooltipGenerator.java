7c7e94244e5a87c1a242cb919586326d8b69eeb3
Jamey <jparham@snjdigitalsolutions.com>
2025-04-21 21:30:44 -0400

    Added builder

--- src/main/java/com/snjdigitalsolutions/springbootutilityfx/node/utility/TooltipGenerator.java
+++ src/main/java/com/snjdigitalsolutions/springbootutilityfx/node/utility/TooltipGenerator.java
@@ -17,4 +17,34 @@ public class TooltipGenerator {
         return generateTooltip(text, 350D);
     }
 
+    public static Builder builder() {
+        Builder builder = new Builder();
+        builder.showDelay(350D);
+        return builder;
+    }
+
+    public static class Builder {
+
+        private final Tooltip tooltip = new Tooltip();
+
+        public Builder text(String text){
+            tooltip.setText(text);
+            return this;
+        }
+
+        public Builder showDelay(Double delay){
+            tooltip.setShowDelay(new Duration(delay));
+            return this;
+        }
+
+        public Builder fontSize(Integer size){
+            tooltip.setStyle(String.format("-fx-font-size: %dpx", size));
+            return this;
+        }
+
+        public Tooltip build(){
+            return tooltip;
+        }
+    }
+
 }
